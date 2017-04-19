ee89a5f856f4cfa8a7daa686591565a4ae6b62ca
Federico Pedemonte <federico.pedemonte@arpal.gov.it>
2017-04-19 10:43:58 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,14 @@
+gnome-shell-extension-xorwayland
+====================================
+
+XorWayland is an Gnome-shell extension which displays an icon to tell if Gnome is running using Xorg or Wayland
+
+### Installation from git
+
+    git clone https://github.com/pedemonte/gnome-shell-extension-xorwayland.git
+    cd gnome-shell-extension-xorwayland
+    mkdir -p ~/.local/share/gnome-shell/extensions
+    cp -r xorwayland@fepede.net ~/.local/share/gnome-shell/extensions/.
+
+if you are running Xorg you should restart GNOME Shell (`Alt+F2`, `r`, `Enter`) and enable the extension through gnome-tweak-tool.
+if you are runninx Wayland you need to logout and re-login (gnome shell restart isn't supported in Wayland yet) and enable the extension through gnome-tweak tool.
---
+++ xorwayland@fepede.net/extension.js
@@ -0,0 +1,45 @@
+const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
+const Main = imports.ui.main;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const St = imports.gi.St;
+
+let statusButton;
+let layout;
+
+function init() {
+    statusButton = new St.Bin({
+        style_class: 'panel-button',
+        x_fill: false,
+        y_fill: false,
+    });
+
+    _updateIcon();
+
+    Gtk.IconTheme.get_default().append_search_path(Me.dir.get_child('icons').get_path());
+
+    layout = new St.BoxLayout();
+    layout.add_actor(statusButton);
+}
+
+function enable() {
+	Main.panel._rightBox.insert_child_at_index(layout, 0);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_child(layout);
+}
+
+function _updateIcon() {
+    let [res, out] = GLib.spawn_command_line_sync("pgrep Xorg");
+
+    if (out.length == 0) {
+        icon = new St.Icon({ icon_name: 'Wayland_Logo', style_class: 'popup-menu-icon' }); 
+        statusButton.set_child(icon);
+    } else {
+        icon = new St.Icon({ icon_name: 'X11_Logo', style_class: 'popup-menu-icon' });
+        statusButton.set_child(icon);
+    }
+
+    return(true);
+}
