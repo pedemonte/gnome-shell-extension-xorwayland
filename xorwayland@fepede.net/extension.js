0f74fe81081c5e983b3333973ba8dbdab73dcdd1
ttyS3 <ttys3.rust@gmail.com>
2024-03-26 11:21:03 +0000

    refactor: port code to GNOME 46
    refs:
    https://gjs.guide/extensions/upgrading/gnome-shell-40.html
    https://gjs.guide/extensions/upgrading/gnome-shell-42.html
    https://gjs.guide/extensions/upgrading/gnome-shell-43.html
    https://gjs.guide/extensions/upgrading/gnome-shell-44.html
    https://gjs.guide/extensions/upgrading/gnome-shell-45.html
    https://gjs.guide/extensions/upgrading/gnome-shell-46.html
    
    https://blogs.gnome.org/shell-dev/2023/09/02/extensions-in-gnome-45/
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/tree/main/js?ref_type=heads
    
    https://gjs.guide/extensions/development/creating.html
    
    https://gjs.guide/extensions/overview/anatomy.html

--- xorwayland@fepede.net/extension.js
+++ xorwayland@fepede.net/extension.js
@@ -1,55 +1,66 @@
-const GLib = imports.gi.GLib;
-const Gio = imports.gi.Gio;
-const Gtk = imports.gi.Gtk;
-const Main = imports.ui.main;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
-const St = imports.gi.St;
+import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
+import Gdk from 'gi://Gdk';
+import St from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-let statusButton;
-let layout;
 
-function init() {
-    statusButton = new St.Bin({
-        style_class: 'panel-button',
-        x_expand: false,
-        y_expand: false,
-    });
+const Me = Extension.lookupByUUID('xorwayland@fepede.net');
 
-    _updateIcon();
+export default class XorWaylandExtension extends Extension {
 
-    layout = new St.BoxLayout();
-    layout.add_actor(statusButton);
-}
+    // see https://gjs.guide/extensions/overview/anatomy.html
+    constructor(metadata) {
+        super(metadata);
+        console.debug(`constructing ${this.metadata.name}`);
 
-function enable() {
-	Main.panel._rightBox.insert_child_at_index(layout, 0);
-}
-
-function disable() {
-    Main.panel._rightBox.remove_child(layout);
-}
-
-function _getIcon(icon_name) {
-    // Check if icon is available in theme
-    // get_default can be null (on Wayland)
-    if (Gtk.IconTheme.get_default() && Gtk.IconTheme.get_default().has_icon(icon_name)) {
-        return Gio.icon_new_for_string(icon_name);
+        this.statusButton = new St.Bin({
+            style_class: 'panel-button',
+            x_expand: false,
+            y_expand: false,
+        });
+    
+        this._updateIcon();
+    
+        this.layout = new St.BoxLayout();
+        this.layout.add_child(this.statusButton);
     }
-    // Icon not available in theme
-    return Gio.icon_new_for_string(Me.dir.get_child('icons').get_path() + "/" + icon_name + ".svg");
-}
-
-function _updateIcon() {
-    //returns x11 or wayland
-    let sessionType = GLib.getenv('XDG_SESSION_TYPE');
-
-    if (sessionType == 'wayland') {
-        icon = new St.Icon({gicon: _getIcon('Wayland_Logo'), style_class: 'popup-menu-icon'});
-    } else {
-        icon = new St.Icon({gicon: _getIcon('X11_Logo'), style_class: 'popup-menu-icon'});
+    
+    enable() {
+        console.debug(`enabling ${this.metadata.name}`);
+        Main.panel._rightBox.insert_child_at_index(this.layout, 0);
     }
-
-    statusButton.set_child(icon);
-
-    return(true);
+    
+    disable() {
+        console.debug(`disabling ${this.metadata.name}`);
+        Main.panel._rightBox.remove_child(this.layout);
+    }
+    
+    _getIcon(icon_name) {
+        // Check if icon is available in theme
+        // get_default can be null (on Wayland)
+        if (Gdk.Display.get_default() && Gtk.IconTheme.get_for_display(Gdk.Display.get_default()) && Gtk.IconTheme.get_for_display(Gdk.Display.get_default()).has_icon(icon_name)) {
+            return Gio.icon_new_for_string(icon_name);
+        }
+        // Icon not available in theme
+        return Gio.icon_new_for_string(this.dir.get_child('icons').get_path() + "/" + icon_name + ".svg");
+    }
+    
+    _updateIcon() {
+        //returns x11 or wayland
+        let sessionType = GLib.getenv('XDG_SESSION_TYPE');
+    
+        if (sessionType == 'wayland') {
+            this.icon = new St.Icon({gicon: this._getIcon('Wayland_Logo'), style_class: 'popup-menu-icon'});
+        } else {
+            this.icon = new St.Icon({gicon: this._getIcon('X11_Logo'), style_class: 'popup-menu-icon'});
+        }
+    
+        this.statusButton.set_child(this.icon);
+    
+        return(true);
+    }
+    
 }
